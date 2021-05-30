cd6bebed2e4572a0cdb3e27714b5405337da228b
paul <piankin250@gmail.com>
2021-05-30 23:18:38 +0300

    text processing

--- TextProcessingWorkshopPython/TextProcessingWorkshopPython.py
+++ TextProcessingWorkshopPython/TextProcessingWorkshopPython.py
@@ -3,41 +3,41 @@ from Rally.TestsAndFoldersActions import TestsAndFoldersActions
 import pickle
 
 #read creads
-params = []
-with open(r'C:\Users\User\Desktop\!temp') as my_file:
-    for line in my_file:
-        params.append(line)
+#params = []
+#with open(r'C:\Users\User\Desktop\!temp') as my_file:
+#    for line in my_file:
+#        params.append(line)
 
-serverText = params[0].rstrip()
-userText = params[1].rstrip()
-passwordText = params[2].rstrip()
-workspaceText = params[3].rstrip()
-projectText = params[4].rstrip()
-rootFolderText = params[5].rstrip() #TF15961 
-queryText = params[6].rstrip()
+#serverText = params[0].rstrip()
+#userText = params[1].rstrip()
+#passwordText = params[2].rstrip()
+#workspaceText = params[3].rstrip()
+#projectText = params[4].rstrip()
+#rootFolderText = params[5].rstrip() #TF15961 
+#queryText = params[6].rstrip()
 
 #connection
-rally = Rally(server=serverText, user=userText, password=passwordText)
+#rally = Rally(server=serverText, user=userText, password=passwordText)
 
 #set project and root folder
-rally.setWorkspace(workspaceText)
-rally.setProject(projectText)
+#rally.setWorkspace(workspaceText)
+#rally.setProject(projectText)
 
 #get information from root folder
-testFolderRequest = rally.get('TestFolder', fetch=True, projectScopeDown=True, query='FormattedID = "' + rootFolderText + '"')
-testFolder = testFolderRequest.next()
+#testFolderRequest = rally.get('TestFolder', fetch=True, projectScopeDown=True, query='FormattedID = "' + rootFolderText + '"')
+#testFolder = testFolderRequest.next()
 
 #get all test cases inside of root folder
-allTestCasesForSaveIntoFile = TestsAndFoldersActions().extractTestCasesFromFoldersAndSubfolders(testFolder)
+#allTestCasesForSaveIntoFile = TestsAndFoldersActions().extractTestCasesFromFoldersAndSubfolders(testFolder)
 
 #save test cases into file
-with open(r'C:\Temp2\New folder\DataVisualizationAndStatisticsForRally\Draft\allTestCasesForSaveIntoFile.data', 'w+b') as file:
-    pickle.dump(allTestCasesForSaveIntoFile, file)
-print("Test cases were saved")
+#with open(r'C:\Temp2\New folder\DataVisualizationAndStatisticsForRally\Draft\allTestCasesForSaveIntoFile.data', 'w+b') as file:
+#    pickle.dump(allTestCasesForSaveIntoFile, file)
+#print("Test cases were saved")
 
 #load test cases from file
+allTestCasesForSaveIntoFile = None
 with open(r'C:\Temp2\New folder\DataVisualizationAndStatisticsForRally\Draft\allTestCasesForSaveIntoFile.data', 'rb') as f:
     allTestCasesForSaveIntoFile = pickle.load(f)
-    r=0
 
 e=0
