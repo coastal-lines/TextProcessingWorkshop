1428aad31ce1c249782e5d3472da0e5f54f509bb
paul <piankin250@gmail.com>
2021-06-01 17:14:56 +0300

    text processing

--- TextProcessingWorkshopPython/Temp.py
+++ TextProcessingWorkshopPython/Temp.py
@@ -3,38 +3,66 @@ from Rally.TestsAndFoldersActions import TestsAndFoldersActions
 import pickle
 from bs4 import BeautifulSoup
 
-#read creads
-params = []
-with open(r'C:\Users\User\Desktop\!temp') as my_file:
-    for line in my_file:
-        params.append(line)
+#
+def preparingData():
+    #read creads
+    params = []
+    with open(r'C:\Users\User\Desktop\!temp') as my_file:
+        for line in my_file:
+            params.append(line)
 
-#serverText = params[0].rstrip()
-#userText = params[1].rstrip()
-#passwordText = params[2].rstrip()
-#workspaceText = params[3].rstrip()
-#projectText = params[4].rstrip()
-#rootFolderText = params[5].rstrip() #TF15961 
-#queryText = params[6].rstrip()
+    #connection
+    rally = Rally(server=params[0].rstrip(), user=params[1].rstrip(), password=params[2].rstrip())
 
-#connection
-rally = Rally(server=params[0].rstrip(), user=params[1].rstrip(), password=params[2].rstrip())
+    #set project and root folder
+    rally.setWorkspace(params[3].rstrip())
+    rally.setProject(params[4].rstrip())
 
-#set project and root folder
-rally.setWorkspace(params[3].rstrip())
-rally.setProject(params[4].rstrip())
+    #get information from root folder
+    testFolderRequest = rally.get('TestFolder', fetch=True, projectScopeDown=True, query='FormattedID = "' + "TF18438" + '"')
+    testFolder = testFolderRequest.next()
 
-#get information from root folder
-testFolderRequest = rally.get('TestFolder', fetch=True, projectScopeDown=True, query='FormattedID = "' + params[5].rstrip() + '"')
-testFolder = testFolderRequest.next()
+    #get all test cases inside of root folder
+    allTestCasesForSaveIntoFile = TestsAndFoldersActions().extractTestCasesFromFoldersAndSubfolders(testFolder)
 
-#get all test cases inside of root folder
-allTestCasesForSaveIntoFile = TestsAndFoldersActions().extractTestCasesFromFoldersAndSubfolders(testFolder)
+    #save test cases into file
+    with open(r'C:\Temp2\New folder\DataVisualizationAndStatisticsForRally\Draft\allTestCasesForSaveIntoFile.data', 'w+b') as file:
+        pickle.dump(allTestCasesForSaveIntoFile, file)
+    print("Test cases were saved")
 
-#save test cases into file
-with open(r'C:\Temp2\New folder\DataVisualizationAndStatisticsForRally\Draft\allTestCasesForSaveIntoFile.data', 'w+b') as file:
-    pickle.dump(allTestCasesForSaveIntoFile, file)
-print("Test cases were saved")
+
+
+def saveLikeAText(listInputs):
+    textfile = open(r"C:\Temp2\New folder\DataVisualizationAndStatisticsForRally\Draft\inputsListTxt.txt", "w")
+    for element in listInputs:
+        textfile.write(element + "\n")
+    textfile.close()
+
+def htmlParser(text):
+    listInputs = []
+    data = BeautifulSoup(text)
+
+    #find all <li>
+    allLi = data.find("body").findAll("li")
+    for li in allLi:
+        listInputs.append(li.text)
+
+    #span
+    allSpan = data.find("body").findAll("span")
+    for span in allSpan:
+        listInputs.append(span.text)
+
+    #div
+    allDiv = data.find("body").findAll("div")
+    for div in allDiv:
+        listInputs.append(div.text)
+
+    #if():
+     #   listInputs.append(text)
+
+    return listInputs
+
+#preparingData()
 
 #load test cases from file
 allTestCasesForSaveIntoFile = None
@@ -46,13 +74,24 @@ with open(r'C:\Temp2\New folder\DataVisualizationAndStatisticsForRally\Draft\all
 inputsList = []
 for tc in allTestCasesForSaveIntoFile:
     for input in tc.inputs:
+        listOfInputsInsideOneStep = htmlParser(input)
+        
         soup = BeautifulSoup(input)
         cleanText = ''.join(soup.findAll(text=True))
         cleanText = cleanText.replace(u'\xa0', ' ')
-        cleanText = cleanText.splitlines()
+        #splittedLines = []
+        #if('\n' in cleanText):
+            #   #splitString(cleanText)
+          #  splittedLines = cleanText.splitlines()
+         #   inputsList.append(splittedLines)
         inputsList.append(cleanText)
-        print(cleanText)
+        inputsList.extend(listOfInputsInsideOneStep)
+        #print(cleanText)
+
+saveLikeAText(inputsList)
 
 #save input list into file
-with open(r'C:\Temp2\New folder\DataVisualizationAndStatisticsForRally\Draft\inputsList.data', 'w+b') as file:
-    pickle.dump(inputsList, file)
+#with open(r'C:\Temp2\New folder\DataVisualizationAndStatisticsForRally\Draft\inputsList.data', 'w+b') as file:
+ #   pickle.dump(inputsList, file)
+
+#72 lines should be
