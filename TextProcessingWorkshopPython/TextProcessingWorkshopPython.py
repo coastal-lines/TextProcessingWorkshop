fd39cdb4099aaee480188731ce87a0e95a175399
paul <piankin250@gmail.com>
2021-06-03 18:58:26 +0300

    text processing

--- TextProcessingWorkshopPython/TextProcessingWorkshopPython.py
+++ TextProcessingWorkshopPython/TextProcessingWorkshopPython.py
@@ -11,6 +11,26 @@ def loadTestCasesFromFile():
     return allTestCasesForSaveIntoFile
 
 def saveInputsIntoTextFile(listInputs):
+    #removing some of extra characters
+    position = -1
+    for line in listInputs:
+        position = position + 1
+
+        if(line == '\xa0'):
+            listInputs.pop(position)
+
+        if('\xa0' in line):
+            line = line.replace('\xa0', ' ')
+            listInputs[position] = line
+
+        #if('\n' in line):
+         #   line = " ".join(line.splitlines())
+          #  listInputs[position] = line
+
+        tempLine = line.strip()
+        if(len(tempLine) == 0):
+            listInputs.pop(position)
+
     textfile = open(r"C:\Temp2\New folder\DataVisualizationAndStatisticsForRally\Draft\inputsListTxt.txt", "w")
     for element in listInputs:
         textfile.write(element + "\n")
@@ -48,11 +68,14 @@ def htmlParser(text):
     listInputs = []
     data = BeautifulSoup(text)
 
+    parentTag = data.find("body").next.name
+
     allLi = data.find("body").findAll("li")
     allSpan = data.find("body").findAll("span")
     allDiv = data.find("body").findAll("div")
+    allP = data.find("body").findAll("p")
 
-    if (allLi != []):
+    if (parentTag == "li" and allLi != []):
         for li in allLi:
             span = li.find_all("span")
             if (span != [] and span[0].text != ""):
@@ -60,13 +83,26 @@ def htmlParser(text):
                 continue
             listInputs.append(li.text)
 
-    elif (allSpan != []):
-        for span in allSpan:
-            listInputs.append(span.text)
+    elif (parentTag == "span" and allSpan != []):
+        if(allDiv == []):
+            for span in allSpan:
+                u = data.findAll("u")
+                strong = data.findAll("strong")
+                if(u != [] or strong != []):
+                    listInputs.append(data.getText())
+                    break
+                listInputs.append(span.text)
 
-    elif (allDiv != []):
+    elif (parentTag == "div" and allDiv != []):
         for div in allDiv:
+            if(div.findAll("span") != []):
+                listInputs.append(data.getText())
+                break
             listInputs.append(div.text)
+
+    elif (parentTag == "p" and allP != []):
+            for p in allP:
+                listInputs.append(p.text)
 
     elif(allLi == [] and allSpan == [] and allDiv == []):
         allP = data.find("body").findAll("p")
@@ -79,8 +115,9 @@ def htmlParser(text):
 def prepareInputsList(allTestCasesForSaveIntoFile):
     inputsList = []
     for tc in allTestCasesForSaveIntoFile:
+        #inputsList.append(tc.formattedID)
         for input in tc.inputs:
-            if(tc.formattedID == "TC40919"):
+            #if(tc.formattedID == "TC999"):
                 listOfInputsInsideOneStep = htmlParser(input)
                 inputsList.extend(listOfInputsInsideOneStep)
 
--- TextProcessingWorkshopPython/module1.py
+++ TextProcessingWorkshopPython/module1.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup
 
-text = '<li class="rally-rte-class-08688b1e92a91">Go to&nbsp;<span class="rally-rte-class-0cba33f787a396">Surpass Editions</span>&nbsp;&gt; Home &gt;Deliver Test &gt; HTML Delivery &gt; Select<span class="rally-rte-class-0cba33f787a396">&nbsp;English&nbsp;</span>language</li>'
+text = '<span style="background-color:#ffffff; font-family:NotoSans,Helvetica,Arial; line-height:17.1429px">Check Media file on the&nbsp;</span><u style="background-color:#ffffff; box-sizing:border-box; font-family:NotoSans,Helvetica,Arial; line-height:17.1429px">Introduction page.</u>'
 data = BeautifulSoup(text)
-liList = data.find_all("li")
+sp = data.find_all("u")
 
 textList = []
 for li in liList:
@@ -13,21 +13,17 @@ for li in liList:
     textList.append(li.text)
 t=0
 
+    position = -1
+    for line in listInputs:
+        position = position + 1
 
+        if(line == '\xa0'):
+            listInputs.pop(position)
 
-#create list of all Inputs
-#remove html tags from inputs
-inputsList = []
-for tc in allTestCasesForSaveIntoFile:
-    for input in tc.inputs:
-        listOfInputsInsideOneStep = htmlParser(input)
-        inputsList.extend(listOfInputsInsideOneStep)
+        if('\n' in line):
+            line = " ".join(line.splitlines())
+            listInputs[position] = line
 
-
-saveLikeAText(inputsList)
-
-#save input list into file
-#with open(r'C:\Temp2\New folder\DataVisualizationAndStatisticsForRally\Draft\inputsList.data', 'w+b') as file:
- #   pickle.dump(inputsList, file)
-
-#72 lines should be
+        tempLine = line.strip()
+        if(len(tempLine) == 0):
+            listInputs.pop(position)
